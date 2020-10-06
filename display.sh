c07ca922253a9ec57168a8eb69a7bdceddea40a7
spin6lock <spin6lock@users.noreply.github.com>
2020-10-06 22:37:31 +0800

    remove \t \n with space

--- display.sh
+++ display.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cat timeline.json | \
-    jq -r '["----------------------------------------"], (. | ["@\(.user.screen_name):\(.full_text) via: https://twitter.com/\(.user.screen_name)/status/\(.id_str)"]) | @tsv ' -C | \
+    jq -r '["----------------------------------------"], (. | ["@\(.user.screen_name):\(.full_text) via: https://twitter.com/\(.user.screen_name)/status/\(.id_str)"]) | @tsv' -C | \
+    sed 's/\\[tn]/ /g'| \
     less -R
