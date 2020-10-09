ac48fe752787c1eb55c7a333e271927ae68750f7
spin6lock <spin6lock@users.noreply.github.com>
2020-10-09 10:14:20 +0800

    .full_text -> .text

--- display.sh
+++ display.sh
@@ -2,5 +2,5 @@
 
 cat timeline.json | \
     sed 's/\\[tn]/ /g'| \
-    jq -r '"-----------------------\n\u001b[33m@\(.user.screen_name):\u001b[0m\t\(.full_text) via: \u001b[34mhttps://twitter.com/\(.user.screen_name)/status/\(.id_str)\u001b[0m"' -C | \
+    jq -r '"-----------------------\n\u001b[33m@\(.user.screen_name):\u001b[0m\t\(.text) via: \u001b[34mhttps://twitter.com/\(.user.screen_name)/status/\(.id_str)\u001b[0m"' -C | \
     less -R
