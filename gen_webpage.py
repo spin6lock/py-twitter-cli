a5b7b6942b2165bfae7eb12f8008cf8223b326e5
spin6lock <spin6lock@users.noreply.github.com>
2022-03-05 18:01:04 +0800

    add img_style and ahref_gen

--- gen_webpage.py
+++ gen_webpage.py
@@ -7,6 +7,16 @@ import utils
 import subprocess
 import re
 
+
+img_style = f"height:{config.image_height}"
+def img_gen(src, style=img_style):
+    return f'<img src={src} style="{style}" >'
+
+
+def ahref_gen(text, link):
+    return f"<a href='{link}' target='_blank'>{text}</a>"
+
+
 def display_one_tweet_image(tweet, fout):
     path = None
     if utils.is_contains_multiple_media(tweet):
@@ -17,22 +27,24 @@ def display_one_tweet_image(tweet, fout):
             url = media.media_url_https
             postfix = url[url.rfind("."):]
             path = os.path.join(parent_path, str(i) + postfix)
-            fout.write('<img src={} style="height:{}">'.format(path, config.image_height))
+            fout.write(img_gen(path))
     else:
         media = tweet.entities.media[0]
         url = media.media_url_https
         postfix = url[url.rfind("."):]
         image_id = utils.gen_image_id(tweet)
         path = os.path.join("images", image_id+postfix)
-        fout.write('<a href={}><img src={} style="height:{}"></a>'.format(path, path, config.image_height))
+        image_tag = img_gen(path)
+        fout.write(ahref_gen(image_tag, path))
 
 
 def get_text_from(tweet):
     text = tweet.full_text
     if tweet.quoted_status:
-        text = text + "\tQuote:" + tweet.quoted_status.full_text
+        text = f"{text} Quote: {tweet.quoted_status.full_text}"
     if tweet.retweeted_status:
-        text = text + "\tRT:" + get_text_from(tweet.retweeted_status)
+        ret_text = get_text_from(tweet.retweeted_status)
+        text = f"{text} RT: {ret_text}"
     return text
 
 
@@ -42,8 +54,9 @@ def add_link_for_text(tweet):
     urls = pattern.findall(str(text))
     if len(urls) > 0:
         text = text.replace(urls[0], '')
-        for url in urls[1:]:
-            text = text.replace(url, f'<a href="{url}">{url}</a>')
+        for url in set(urls[1:]):
+            url_link = ahref_gen(url, url)
+            text = text.replace(url, url_link)
     return text
 
 
@@ -55,7 +68,10 @@ def display(timeline):
             fout.write("<tr><td>")
             link = f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id_str}"
             text = add_link_for_text(tweet)
-            fout.write(f'<a href="https://twitter.com/{tweet.user.screen_name}">@{tweet.user.screen_name}</a>: {text} <a href="{link}">source</a>')
+            name_link = f"https://twitter.com/{tweet.user.screen_name}"
+            tweet_head = ahref_gen(f"@{tweet.user.screen_name}", name_link)
+            source = ahref_gen("source", link)
+            fout.write(f'{tweet_head}: {text} {source}')
             if tweet.entities.media:
                 display_one_tweet_image(tweet, fout)
             fout.write("</tr></td>")
